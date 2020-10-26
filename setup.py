e098ab261caea895f7638bd75643c46424dc1c24
Vyom Jain <vyom.j@protonmail.com>
2020-10-26 14:06:21 -0500

    fix(*)

--- setup.py
+++
@@ -1,8 +0,0 @@
-"""
-This is a setup file, where you may add the token for hosting this bot as your own.
-If you don't know have a discord token, go to https://discord.com/developers/applications and get your own and assign it below.
-"""
-
-TOKEN = "YOUR_TOKEN"
-USER_NAME = "disbot"
-MONGODB_PASS = "14ZrL5tjTyJknv6q"
