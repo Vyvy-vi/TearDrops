0e21d045c108ba42142ccbf283d9514c2c24181a
Vyom Jain <vyom.j@protonmail.com>
2020-10-26 13:45:51 -0500

    feat(*): added botinfo

--- main.py
+++ main.py
@@ -396,6 +396,21 @@ async def shop(ctx):
 """
 
 
+@client.command(aliases=['botwhat'])
+async def botinfo(ctx):
+    '''Gives info about the bot'''
+    embed = discord.Embed(title='**Tear Drops:tm:**', description='A dynamic bot for _crying_, entertainment, economy and _other_ purposes...\n\
+I am here to reek sorrow and depression. Come let\'s cry together 😢\
+The prefix for the bot is _"qq"_, cuz you know _"less qq, more pew pew..."_ \
+The currency credits for the bot are _tears_(hahah obviously). Have fun being sad...\
+\nNOTE- Even though this is OpenSource and under MIT license, I request you to not start a commercial bot with the same name "Tear Drops:tm:"\
+This bot is under MIT License(provided as is, do whatever you want) \
+This has been uploaded to GitHub for educational and referencial purposes', colour=discord.Color.purple(), url='https://github.com/Py-Contributors/awesomeScripts/Tear-Drops_DiscordBot/')
+    embed.set_footer(text='I Hope that you enjoyed the bot....😭')
+    embed.set_image(url='https://cdn.discordapp.com/attachments/582605227081990233/627388598181953548/unknown.png')
+    await ctx.send(embed=embed)
+
+
 # error_handling
 
 
