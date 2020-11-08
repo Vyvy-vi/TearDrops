4510fbda2be5e3d1b1e8ac6c00cae5229621a1de
TJ LeBlanc <ssjarceus@outlook.com>
2020-11-07 20:31:40 -0500

    fix wiki command issues

--- bot/main.py
+++ bot/main.py
@@ -547,18 +547,17 @@ async def russian_roulette(ctx):
 
 
 @client.command(pass_context=True)
-async def wiki(ctx, *args):
+async def wiki(ctx, *, args):
     '''Displays wikipedia info about given arguments'''
-    qu = ' '.join(list(args))
-    searchResults = wikipedia.search(qu)
+    searchResults = wikipedia.search(args)
     if not searchResults:
         embed = discord.Embed(
-            title=f'**{qu}**', description='It appears that there is no instance of this in Wikipedia index...', colour=discord.Color.dark_red())
+            title=f'**{args}**', description='It appears that there is no instance of this in Wikipedia index...', colour=discord.Color.dark_red())
         embed.set_footer(text='Powered by Wikipedia...')
         await ctx.send(embed=embed)
     else:
         try:
-            page = wikipedia.page(searchResults[0])
+            page = wikipedia.page(searchResults[0], auto_suggest=False)
             pg = 0
         except wikipedia.DisambiguationError as err:
             page = wikipedia.page(err.options[0])
