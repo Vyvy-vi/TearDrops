fc115e4f5afbdcf7e8ec8edcc5b193184ed6b353
Vyvy-vi <vyom.j@protonmail.com>
2020-11-14 01:45:54 +0530

    feat: added events cog

--- bot/main.py
+++ bot/main.py
@@ -73,7 +73,8 @@ ls_cog = ['cogs.fun_cog',
           'cogs.utils_cog',
           'cogs.name_cog',
           'cogs.game_cog',
-          'cogs.economy_cog']
+          'cogs.economy_cog',
+          'cogs.events_cog']
 
 
 @client.event
@@ -92,37 +93,6 @@ async def on_ready():
     # client.user gives the bots discord username tag
 
 
-@client.event
-async def on_guild_join(guild):
-    '''
-    This sends a message in the main channel, when the bot joins a guild.
-    Joining a guild is synonymous to joining a server.
-    Basically, a hi message the bot sends on enterring the server.
-    '''
-
-    for channel in guild.text_channels:
-        if channel.permissions_for(guild.me).send_messages:
-            if guild.id not in db.list_collection_names():
-                col = db[str(guild.id)]
-                col.insert_one(
-                    {'server_name': guild.name, 'server_id': guild.id})
-            icon_url = 'https://cdn.discordapp.com/attachments/582605227081990233/627388598181953548/unknown.png'
-            embed = discord.Embed(
-                title='**Tear Drops:tm:**',
-                description='A dynamic bot for _crying_, entertainment, economy and _other_ purposes...\n\
-I am here to reek sorrow and depression. Come let\'s cry together 😢\
-The prefix for the bot is _"qq"_, cuz you know _"less qq, more pew pew..."_ \
-The currency credits for the bot are _tears_(hahah obviously). Have fun being sad...\
-\nNOTE- Even though this is OpenSource and under MIT license, I request you to not start a commercial bot with the same name "Tear Drops:tm:"\
-This bot is under MIT License(provided as is, do whatever you want) \
-This has been uploaded to GitHub for educational and referencial purposes',
-                colour=discord.Color.purple(),
-                url='https://github.com/Py-Contributors/awesomeScripts/Tear-Drops_DiscordBot/')
-            embed.set_footer(text='I Hope that you enjoyed the bot....😭')
-            embed.set_image(url=icon_url)
-            await channel.send(embed=embed)
-        break
-    print(f'Entered server {guild.name} : {guild.id}')
 
 
 @tasks.loop(seconds=600)
@@ -135,13 +105,6 @@ async def change_status():
     # 'playing'
 
 
-@client.event
-async def on_member_remove(member):
-    '''
-    Event triggered when a member leaves the server
-    NOTE- This can also be displayed on the server
-    '''
-    print(f'{member} has left the server......')
 
 
 # error_handling
