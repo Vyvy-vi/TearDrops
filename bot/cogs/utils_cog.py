02bfa00c038176f1f654a9324398e52de8b6b1df
Vyvy-vi <vyom.j@protonmail.com>
2020-11-25 18:54:12 +0530

    fix: fixed intents to allow user status reporting

--- bot/cogs/utils_cog.py
+++ bot/cogs/utils_cog.py
@@ -48,20 +48,22 @@ class Utils(commands.Cog):
         await ctx.send(baseurl + output)
 
     @commands.command(pass_context=True)
-    async def user(self, ctx, user: discord.Member):
+    async def user(self, ctx, username: discord.Member = None):
+        if username == None:
+            username = ctx.message.author
         '''gives user info'''
         embed = discord.Embed(
             title="{}'s info".format(
-                user.name),
+                username.name),
             description="Here's what I could find.",
             color=0x00ff00)
-        embed.add_field(name="Name", value=user.name, inline=True)
-        embed.add_field(name="ID", value=user.id, inline=True)
-        embed.add_field(name="Status", value=user.status, inline=True)
-        embed.add_field(name="Highest role", value=user.top_role)
-        embed.add_field(name="Joined", value=user.joined_at)
-        embed.add_field(name='Account Created on', value=user.created_at)
-        embed.set_thumbnail(url=user.avatar_url)
+        embed.add_field(name="Name", value=username.name, inline=True)
+        embed.add_field(name="ID", value=username.id, inline=True)
+        embed.add_field(name="Status", value=username.status, inline=True)
+        embed.add_field(name="Highest role", value=username.top_role)
+        embed.add_field(name="Joined", value=username.joined_at)
+        embed.add_field(name='Account Created on', value=username.created_at)
+        embed.set_thumbnail(url=username.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
